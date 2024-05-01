2860458cdaf37b09d4b78b70fdec316aad4f4869
Duc Anh <ducanh372000@gmail.com>
2024-05-01 13:33:07 +0700

    change5_1

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-// var swiper = new Swiper(".mySwiper", {
-//     spaceBetween: 50,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         revEl: ".swiper-button-prev",
-//       },
-//   });
-  var swiper2 = new Swiper(".mySwiper2", {
-    direction: "vertical",
-    // spaceBetween: 50,
-    // pagination: {
-    //   el: ".swiper-pagination",
-    //   clickable: true,
-    // },
-  });
-  var swiper3 = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+var swiper2 = new Swiper(".mySwiper2", {
+  direction: "vertical",
+  // freeMode: true,
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+      mousewheel: true,
+});
+var swiper3 = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
