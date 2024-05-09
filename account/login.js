6b1187f1ac2219240aa6e2fdf4d891e7255352d6
Duc Anh <ducanh372000@gmail.com>
2024-05-10 01:20:52 +0700

    change

--- account/login.js
+++ account/login.js
@@ -1,25 +1,3 @@
-// var swiper2 = new Swiper(".mySwiper2", {
-//     direction: "vertical",
-//         scrollbar: {
-//           el: ".swiper-scrollbar",
-//         },
-//         pagination: {
-//           el: ".swiper-pagination",
-//           clickable: true,
-//         },
-//         // autoplay: {
-//         //   delay: 3000,
-//         //   disableOnInteraction: false,
-//         // },
-//         mousewheel: true,
-//   });
-//   var swiper3 = new Swiper(".mySwiper", {
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
-
 // dropDownMenu
 function dropDownList() {
   // document.getElementById("Linklist-3").classList.toggle("showMenu");
