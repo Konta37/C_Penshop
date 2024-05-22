b459cb400cf541818468982120d52e335f67512b
Duc Anh <ducanh372000@gmail.com>
2024-05-23 01:55:24 +0700

    change

--- user/js/register.js
+++ user/js/register.js
@@ -1,4 +1,3 @@
-
 // dropDownMenu
 function dropDownList() {
   // document.getElementById("Linklist-3").classList.toggle("showMenu");
@@ -33,7 +32,6 @@ function dropDownList8() {
   document.getElementById("Linklist-8").classList.toggle("showMenu");
 }
 
-
 let form = document.getElementById("CustomerRegisterForm");
 let cfError = document.querySelectorAll(".cf-error");
 console.log(cfError);
@@ -60,9 +58,8 @@ form.onsubmit = function (e) {
   } else if (firstname.length < 2) {
     errorMessage = `<li>First name must be more than 1 character</li>`;
     cfError[0].innerHTML = errorMessage;
-  }
-  else{
-    cfError[0].innerHTML ="";
+  } else {
+    cfError[0].innerHTML = "";
   }
 
   if (!lastname) {
@@ -71,9 +68,8 @@ form.onsubmit = function (e) {
   } else if (lastname.length < 2) {
     errorMessage = `<li>First name must be more than 1 character</li>`;
     cfError[1].innerHTML = errorMessage;
-  }
-  else{
-    cfError[1].innerHTML ="";
+  } else {
+    cfError[1].innerHTML = "";
   }
 
   if (!usermail) {
@@ -82,32 +78,29 @@ form.onsubmit = function (e) {
   } else if (!emailRegex.test(usermail)) {
     errorMessage = `<li>Please provide a valid email address</li>`;
     cfError[2].innerHTML = errorMessage;
+  } else {
+    cfError[2].innerHTML = "";
   }
-  else{
-    cfError[2].innerHTML ="";
-  }
-  if (!userpass){
+  if (!userpass) {
     errorMessage = `<li>Must not be blank</li>`;
     cfError[3].innerHTML = errorMessage;
-  }else if(userpass.length <6){
+  } else if (userpass.length < 6) {
     errorMessage = `<li>First name must be more than 6 character</li>`;
     cfError[3].innerHTML = errorMessage;
-  }else{
+  } else {
     cfError[3].innerHTML = "";
   }
-  if (userpasscheck !=userpass){
+  if (userpasscheck != userpass) {
     errorMessage = `<li>Must not be different form password</li>`;
     cfError[4].innerHTML = errorMessage;
-  }
-  else{
+  } else {
     cfError[4].innerHTML = "";
   }
   if (!userphone) {
     errorMessage = `<li>Must not be blank</li>`;
     cfError[5].innerHTML = errorMessage;
-  }
-  else{
-    cfError[5].innerHTML ="";
+  } else {
+    cfError[5].innerHTML = "";
   }
   if (!userdate) {
     errorMessage = `
@@ -120,36 +113,40 @@ form.onsubmit = function (e) {
     </li>
     `;
     cfError[6].innerHTML = errorMessage;
-  }
-  else{
-    cfError[6].innerHTML ="";
+  } else {
+    cfError[6].innerHTML = "";
   }
 
   let userList = JSON.parse(localStorage.getItem("userList")) || [];
   //up len local storage
-  if(emailRegex.test(usermail) && firstname && lastname 
-  && userphone && userdate 
-  && userpasscheck === userpass ){
-    let user = userList.find(function (el,i){
+  if (
+    emailRegex.test(usermail) &&
+    firstname &&
+    lastname &&
+    userphone &&
+    userdate &&
+    userpasscheck === userpass
+  ) {
+    let user = userList.find(function (el, i) {
       return el.email === usermail;
     });
-    if (!user){
-      let id =1;
+    if (!user) {
+      let id = 1;
       let status = true;
       let role = "user";
-      if (userList.length >0){
+      if (userList.length > 0) {
         id = userList[userList.length - 1].id + 1;
       }
-      let newUser ={
+      let newUser = {
         id,
-        name: firstname +" " + lastname,
+        name: firstname + " " + lastname,
         email: usermail,
         password: userpass,
         gender: gender,
         dateofBirth: userdate,
         phone: userphone,
         status: status,
-        role:role,
+        role: role,
       };
       userList.push(newUser);
       localStorage.setItem("userList", JSON.stringify(userList));
@@ -162,8 +159,7 @@ form.onsubmit = function (e) {
       }).then(function () {
         window.location.href = "../../login.html";
       });
-    }
-    else {
+    } else {
       Swal.fire({
         icon: "error",
         title: "Oops...",
